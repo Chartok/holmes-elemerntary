3bf6dd48d05d458ba50e2cd547b1615fb8f86f2c
Chartok <m.bhimjee@outlook.com>
2023-07-11 23:48:28 -0500

    defined GQL queries&mutations

--- server/models/User.js
+++ server/models/User.js
@@ -36,7 +36,7 @@ const userSchema = new Schema(
 userSchema.pre('save', async function (next) {
   if (this.isNew || this.isModified('password')) {
     const saltRounds = 10;
-    this.password = await bcrypt.hash(this.password, saltRounds);
+    this.password = bcrypt.hash(this.password, saltRounds);
   }
 
   next();
