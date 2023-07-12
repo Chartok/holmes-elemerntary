2abf66d36570a7d8084b377a640214a3b332eaa8
Chartok <m.bhimjee@outlook.com>
2023-07-11 23:25:43 -0500

    configured: resolvers & server.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,79 @@
-const { Book, User } = require('../models');
+const { User } = require('../models');
+const signToken = require('../utils/auth');
 
 const resolvers = {
     Query: {
-        
+        // Query single user by id or username
+        user: async (_, { _id, username }) => {
+            return User.findOne({
+                $or: [{ _id: id }, { username }],
+            });
+        },
+    },
+    Mutation: {
+        // Create new user with signed token and send it to client
+        createUser: async (_, { input }) => {
+            const user = await User.create(input);
+
+            if(!user) {
+                throw new Error('Something went wrong!');
+            }
+
+            const token = signToken(user);
+
+            return { token, user };
+
+        },
+
+        // Login user with email and password and send token to client
+        login: async (_, { input }) => {
+            const user = await User.findOne({ $or: [{ username: input.username }, { email: input.email }] });
+
+            if(!user) {
+                throw new Error('Cannot find this user!');
+            }
+
+            const correctPw = await user.isCorrectPassword(input.password);
+
+            if(!correctPw) {
+                throw new Error('Wrong password!');
+            }
+
+            const token = signToken(user);
+
+            return { token, user };
+        },
+
+        // Save book to user's `savedBooks` field by adding it to the set (to prevent duplicates)
+        saveBook: async (_, { userId, book }) => {
+            const updatedUser = await User.findOneAndUpdate(
+                { _id: userId },
+                { $addToSet: { savedBooks: book } },
+                { new: true, runValidators: true }
+            );
+
+            if (!updatedUser) {
+                throw new Error("Could not save book!");
+            }
+
+            return updatedUser;
+        },
+
+        // Remove book from user's `savedBooks` field by removing it from the set
+        removeBook: async (_, { userId, bookId }) => {
+            const updatedUser = await User.findOneAndUpdate(
+                { _id: userId },
+                { $pull: { savedBooks: { bookId } } },
+                { new: true }
+            );
+
+            if (!updatedUser) {
+                throw new Error("Could not remove book!");
+            }
+
+            return updatedUser;
+        },
+    },
+};
+
+module.exports = resolvers;
--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +20,17 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// Create instance of Apollo server with GraphQL schema
+const startApollo = async () => {
+  await server.start();
+  server.applyMiddleware({ app });
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API initialized on localhost:${PORT}`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
+    });
+  });
+};
+
+startApollo();
