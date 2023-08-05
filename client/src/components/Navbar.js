4f9452b5dc648563ca5b5cbfb9de203857ea7d38
Chartok <m.bhimjee@outlook.com>
2023-08-05 01:04:21 -0500

    moved login/signup to pages folder; refactored login

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar, Nav, Container, Modal, Tab } from 'react-bootstrap';
-import SignUpForm from './SignupForm';
-import LoginForm from './LoginForm';
+import SignUpForm from '../pages/SignupForm';
+import LoginForm from '../pages/LoginForm';
 
 import Auth from '../utils/auth';
 
