9453ceb9fc8a879b061ef4d4c35a04b92ed3c11d
Chartok <m.bhimjee@outlook.com>
2023-08-05 17:24:49 -0500

    checking authcontext import/export

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { useMutation, useLazyQuery } from '@apollo/client';
-import { QUERY_ME } from './utils/queries';
-import { REMOVE_BOOK } from './utils/mutations';
-import { AuthContext } from './authContext';
+import { QUERY_ME } from '../utils/queries';
+import { REMOVE_BOOK } from '../utils/mutations';
+import { useAuth } from '../authContext';
 
 function SavedBooks() {
   const { loading, data, error } = useLazyQuery(QUERY_ME);
   const [removeBook] = useMutation(REMOVE_BOOK);
-  const { user } = AuthContext();
+  const { user } = useAuth();
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
