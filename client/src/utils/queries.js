65e938f6c5496c33f9b06c28dba3e8f4eb62a358
Chartok <m.bhimjee@outlook.com>
2023-08-05 03:01:09 -0500

    refactored SearchBooks page

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -2,7 +2,7 @@ import { gql } from '@apollo/client';
 
 // Query to fetch logged-in user's data (including saved books)
 export const QUERY_ME = gql`
-    query me {
+    query user {
         _id
         username
         email
@@ -15,5 +15,17 @@ export const QUERY_ME = gql`
             image
             link
         }
+    }
+`;
+
+// Query to fetch books from Google Books API
+export const QUERY_BOOK = gql`
+    query book {
+        bookId
+        authors
+        description
+        title
+        image
+        link
     }
 `;
