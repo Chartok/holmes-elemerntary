728b7030872be2041c85cb3bf1747a02322bd8f1
Chartok <m.bhimjee@outlook.com>
2023-08-15 20:41:57 -0500

    formatting server.js

--- server/server.js
+++ server/server.js
@@ -23,8 +23,6 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
