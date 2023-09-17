c70706e713440de66b7f7352fa12e75b4fddba30
Chartok <m.bhimjee@outlook.com>
2023-09-17 09:30:05 -0500

    refactored to pass through apollo instance with graphql models and queries

--- server/server.js
+++ server/server.js
@@ -1,21 +1,50 @@
+require('dotenv').config();
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
+const { typeDefs, resolvers } = require('./schemas');
+const context = require('./utils/auth');
+const mongoose = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// Apollo Server Instance with defined typeDefs and resolvers; using context for user Auth
+const server = new ApolloServer({
+	typeDefs,
+	resolvers,
+	context: async ({ req }) => {
+		const user = context({ req });
+		return { user };
+	},
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+	app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// Initialize apollo server with mongoDB instance; open channel for client/sever interaction
+const startApollo = async () => {
+	await server.start();
+	server.applyMiddleware({ app, path: '/' });
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+	// Connection to mongoose..
+	mongoose.connection.once('open', () => {
+		console.log('Connection to mongoose successful...');
+		app.use((err, req, res, next) => {
+			console.log(err);
+			res.status(500).send('The servers could not connect to mongoose...');
+		});
+    // Client connected to server
+		app.listen(PORT, () => {
+			console.log(`Now listening on localhost:${PORT}`);
+			console.log(
+				`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+			);
+		});
+	});
+};
