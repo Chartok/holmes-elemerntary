9a6202a9ff8eb73dc5f2b2860006d48c6c3f254e
Chartok <m.bhimjee@outlook.com>
2023-09-19 12:02:42 -0500

    refactored login/signup

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,92 +1,55 @@
 // see SignupForm.js for comments
-import React, { useState } from 'react';
-import { Form, Button, Alert } from 'react-bootstrap';
+import { useContext, useState } from 'react';
+import { AuthContext } from '../context/authContext';
+import { useForm } from '../utils/formHook';
+import { useMutation } from '@apollo/react-hooks';
 
-import { loginUser } from '../utils/API';
-import Auth from '../utils/auth';
+import { Button, TextField, Container, Stack, Alert } from '@mui/material';
 
-const LoginForm = () => {
-  const [userFormData, setUserFormData] = useState({ email: '', password: '' });
-  const [validated] = useState(false);
-  const [showAlert, setShowAlert] = useState(false);
+import { useNavigate } from 'react-router-dom';
+import { LOGIN_USER } from '../utils/mutations';
 
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setUserFormData({ ...userFormData, [name]: value });
+function Login() {
+  let navigate = useNavigate();
+  const Context = useContext( AuthContext );
+  const [ errors, setErrors ] = useState( [] );
+
+  const loginUserCallback = () => {
+    console.log( 'Logging in user...' );
+    loginUser();
   };
 
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
+  const { onChange, onSubmit, values } = useForm( loginUserCallback, {
+    email: '',
+    password: '',
+  } );
 
-    // check if form has everything (as per react-bootstrap docs)
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
-
-    try {
-      const response = await loginUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
-    } catch (err) {
-      console.error(err);
-      setShowAlert(true);
-    }
-
-    setUserFormData({
-      username: '',
-      email: '',
-      password: '',
-    });
-  };
+  const [ loginUser ] = useMutation( LOGIN_USER, {
+    update( _, { data: { loginUser: userData } } ) {
+      Context.login( userData );
+      navigate( '/searchBooks' );
+    },
+    onError( { graphQLErrors } ) {
+      setErrors( graphQLErrors );
+    },
+    variables: { loginInput: values },
+  } );
 
   return (
-    <>
-      <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
-        <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
-          Something went wrong with your login credentials!
-        </Alert>
-        <Form.Group className='mb-3'>
-          <Form.Label htmlFor='email'>Email</Form.Label>
-          <Form.Control
-            type='text'
-            placeholder='Your email'
-            name='email'
-            onChange={handleInputChange}
-            value={userFormData.email}
-            required
-          />
-          <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
-        </Form.Group>
-
-        <Form.Group className='mb-3'>
-          <Form.Label htmlFor='password'>Password</Form.Label>
-          <Form.Control
-            type='password'
-            placeholder='Your password'
-            name='password'
-            onChange={handleInputChange}
-            value={userFormData.password}
-            required
-          />
-          <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
-        </Form.Group>
-        <Button
-          disabled={!(userFormData.email && userFormData.password)}
-          type='submit'
-          variant='success'>
-          Submit
-        </Button>
-      </Form>
-    </>
+    <Container spacing={ 2 } maxWidth="sm">
+      <h3>Login</h3>
+      <p>Enter your email and password to log in</p>
+      <Stack spacing={ 2 } paddingBottom={ 2 }>
+        <TextField label="Email" name="email" onChange={ onChange } />
+        <TextField label="Password" name="password" onChange={ onChange } />
+      </Stack>
+      { errors.map( errors => (
+        <Alert severity="error">{ errors.message }</Alert>
+      ) ) }
+      <Button variant="contained" onClick={ onSubmit }>Login</Button>
+      <Button variant="contained" onClick={ () => navigate( '/signup' ) }>Not a member? Sign up here!</Button>
+    </Container>
   );
 };
 
-export default LoginForm;
+export default Login;
