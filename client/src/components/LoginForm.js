bbec47b8880bd002cc39c54951b58220b3324aa5
Chartok <m.bhimjee@outlook.com>
2023-09-25 00:21:04 -0500

    fixing authContext user_id from saving books and rendering savedbooks collection

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,9 +1,7 @@
-// see SignupForm.js for comments
-import { useContext, useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { AuthContext } from '../context/authContext';
 import { useForm } from '../utils/hooks';
 import { useMutation } from '@apollo/react-hooks';
-
 import { Button, TextField, Container, Stack, Alert } from '@mui/material';
 
 import { useNavigate } from 'react-router-dom';
@@ -12,7 +10,7 @@ import { LOGIN_USER } from '../utils/mutations';
 function Login() {
   let navigate = useNavigate();
   const Context = useContext(AuthContext);
-  const [errors, setErrors] = useState([]);
+  const [ errors, setErrors ] = useState([]);
 
   const loginUserCallback = () => {
     console.log('Logging in user...');
@@ -24,31 +22,35 @@ function Login() {
     password: '',
   });
 
-  const [loginUser] = useMutation(LOGIN_USER, {
+  const [ loginUser ] = useMutation(LOGIN_USER, {
     update(_, { data: { loginUser: userData } }) {
+      localStorage.setItem('token', userData.token);
       Context.login(userData);
       navigate('/home');
     },
     onError({ graphQLErrors }) {
-      setErrors(graphQLErrors);
+      setErrors([ ...graphQLErrors ]);
     },
     variables: { loginInput: values },
   });
 
   return (
-    <Container spacing={2} maxWidth="sm">
-      <h3>Login</h3>
-      <Stack spacing={2} paddingBottom={2}>
-        <TextField label="Email" name="email" onChange={onChange} />
-        <TextField label="Password" name="password" onChange={onChange} />
-      </Stack>
-      <p>Enter your email and password to log in</p>
-      {errors.map(errors => (
-        <Alert severity="error">{errors.message}</Alert>
-      ))}
-      <Button variant="contained" onClick={onSubmit}>Login</Button>
-      <Button variant="contained" onClick={() => navigate('/signup')}>Not a member? Sign up here!</Button>
-    </Container>
+    <>
+      <Container spacing={ 2 }>
+        <h3>Login</h3>
+        <Stack spacing={ 2 } paddingBottom={ 2 }>
+          <TextField label="Email" name="email" onChange={ onChange } />
+          <TextField label="Password" name="password" onChange={ onChange } />
+        </Stack>
+        <p>Enter your email and password to log in</p>
+        { errors.map(errors => (
+          <Alert severity="error">{ errors.message }</Alert>
+        )) }
+        <Button variant="contained" onClick={ onSubmit }>Login</Button>
+        <Button variant="contained" onClick={ () => navigate('/signup') }>Not a member? Sign up here!</Button>
+      </Container>
+    </>
+
   );
 };
 
--- client/src/context/authContext.js
+++ client/src/context/authContext.js
@@ -1,33 +1,33 @@
+// @ts-nocheck
 import React, { createContext, useReducer } from 'react';
 import jwtDecode from 'jwt-decode';
 
-const initialState = {
-    user: null,
-};
-
 // Check for 'null' token before decoding
 const token = localStorage.getItem("token");
+
+/** @type {{ user: { exp: number } | null }} */
+const initialState = {
+    user: token,
+};
+
 if (token) {
-    /** @type {{ exp:number }} */
+    /** @type {{ exp: number }} */
     const decodedToken = jwtDecode(token);
     // Decode token and initialize user state if token is valid
-    if (localStorage.getItem("token")) {
-        const decodedToken = jwtDecode(localStorage.getItem("token"));
-
-        if (decodedToken.exp * 10000 < Date.now()) {
+        if (decodedToken.exp * 1000 < Date.now()) {
             localStorage.removeItem("token");
             // TODO: Add function to refresh token
         } else {
             initialState.user = decodedToken;
         }
-    }
+    
 }
 
 
 const AuthContext = createContext({
-    user: null,
-    login: (userData) => { },
-    logout: () => { }
+    user: token,
+    login: (userData) => {},
+    logout: () => {}
 });
 
 function authReducer(state, action) {
@@ -48,7 +48,10 @@ function authReducer(state, action) {
     }
 }
 
-function AuthProvider(props) {
+/**
+ * @param {React.JSX.IntrinsicAttributes & React.ProviderProps<{ user: any; login: (userData: any) => void; logout: () => void; }>} props
+ */
+function AuthProvider({ children }) {
     const [ state, dispatch ] = useReducer(authReducer, initialState);
 
     const login = (userData) => {
@@ -59,17 +62,18 @@ function AuthProvider(props) {
         });
     };
 
-    function logout() {
+    const logout = () => {
         localStorage.removeItem("token");
         dispatch({ type: "LOGOUT" });
         // TODO: Inform user they've been logged out
     }
 
+
     return (
         <AuthContext.Provider
-            value={ { user: state.user, login, logout } }
-            { ...props }>{ props.children }
-        </AuthContext.Provider>
+            value = {{ user: state.user, login, logout }}>
+                {children}
+            </AuthContext.Provider>
     );
 }
 
